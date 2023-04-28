c662f32d1a2a1aca21c05f9788c28037cc5bbfed
Mara Sliskovic <mara.sliskovic@akvelon.com>
2023-04-28 10:36:22 +0200

    edit typeahead component

--- src/app/typeahead/typeahead/typeahead.component.ts
+++ src/app/typeahead/typeahead/typeahead.component.ts
@@ -1,18 +1,23 @@
-import { query } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable, Subject, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap,
+} from 'rxjs';
 import { CategoryService } from 'src/app/core/category.service';
 import { WikipediaService } from 'src/app/core/wikipedia.service';
 import { Wikipedia } from 'src/app/shared/wikipedia.model';
-interface PeopleData {
-  name: string;
-  birth_year: string;
-  height: number;
-  weight: number;
-  eyeColor: string;
-}
+
 export interface Category {
   id: number;
   name: string;
@@ -21,95 +26,81 @@ export interface Category {
 @Component({
   selector: 'app-typeahead',
   templateUrl: './typeahead.component.html',
-  styleUrls: ['./typeahead.component.css']
+  styleUrls: ['./typeahead.component.css'],
 })
-export class TypeaheadComponent  implements OnInit {
+export class TypeaheadComponent implements OnInit {
   searchTerm = '';
   filteredCategories: Category[] = [];
   allCategories: Category[] = [];
 
-  allWkipedia: Wikipedia[]= [];
+  allWkipedia: Wikipedia[] = [];
 
- 
   private searchTerms$ = new Subject<string>();
-  private searchWikipedia$ = new Subject<string>();
+  // private searchWikipedia$ = new Subject<string>();
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private categoryService: CategoryService, private wikipediaService: WikipediaService) { 
-    // this.form = this.fb.group({
-    //   category: ['', Validators.required],
-    //   searchInput: ['', Validators.required]
-    // });
-    // this.form.controls['searchInput'].valueChanges
-    // .pipe(debounceTime(500))
-    // .subscribe(query => {
-    //   if (query && query.length > 2) {
-    //     // this.searchWikipedia();
-    //   }
-    // });
-  }
-  
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService,
+    private wikipediaService: WikipediaService
+  ) { }
+
   searchInput = new FormControl();
-  category = new  FormControl();
+  category = new FormControl();
   form = new FormGroup({
     searchInput: this.searchInput,
-    category: this.category
-  })
- 
-  
+    category: this.category,
+  });
+
   ngOnInit(): void {
-   this.categoryService.getCategories().subscribe(res=> {
-    this.allCategories = res;
-   })
-
-  
-
-  }
-
-  searchCategories(term:string): Observable<Category[]>{
-    return this.categoryService.getCategories().pipe(
-      map(res=>res.filter(category=>category.name.toLowerCase().includes(term)))
-    )
-  }
-  searchWikipedia(){
-    const category = this.form.controls['category'].value;
-    const searchInput = this.form.controls['searchInput'].value;
-    this.wikipediaService.searchWikipedia(searchInput, category).subscribe(response => {
-      this.allWkipedia = response;
-      console.log(this.allWkipedia);
-      
+    this.categoryService.getCategories().subscribe((res) => {
+      this.allCategories = res;
     });
- }
-
- onInputChange() {
-  this.form.controls.searchInput.valueChanges.pipe(
-    debounceTime(500),
-    distinctUntilChanged(),
-   ).subscribe(searchInput => {
-    if (searchInput && searchInput.length > 2) {
-      this.searchWikipedia();
-    }
-   })
-  // const query = this.form.controls['searchInput'].value;
-  // this.searchWikipedia$.next(query);
-  // this.searchWikipedia$.pipe(
-  //   debounceTime(500),
-  //   distinctUntilChanged(),
-  //   switchMap((term:string)=>this.searchCategories(term))
-  // )
- }
-
-  onSearch(){
-    this.searchTerms$.next(this.searchTerm);
-    this.searchTerms$.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      switchMap((term:string)=>this.searchCategories(term))
-    ).subscribe((results: Category[])=> {
-      if (results && results.length > 2)
-      console.log(results);
-      this.filteredCategories = results;
-    })
   }
 
-  
+  searchWikipedia() {
+    const category = this.form.controls.category.value;
+    const searchInput = this.form.controls.searchInput.value;
+    console.log(searchInput);
+
+    this.wikipediaService
+      .searchWikipedia(searchInput, category)
+      .subscribe((response) => {
+        this.allWkipedia = response;
+        console.log(this.allWkipedia);
+      });
+  }
+
+  onInputChange() {
+    this.form.controls.searchInput.valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((searchInput) => {
+        if (searchInput && searchInput.length > 2) {
+          this.searchWikipedia();
+        }
+      });
+  }
+
+  // Search categories
+  onSearch() {
+    this.searchTerms$.next(this.searchTerm);
+    this.searchTerms$
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((term: string) => this.searchCategories(term))
+      )
+      .subscribe((results: Category[]) => {
+        if (results && results.length > 2) console.log(results);
+        this.filteredCategories = results;
+      });
+  }
+  searchCategories(term: string): Observable<Category[]> {
+    return this.categoryService
+      .getCategories()
+      .pipe(
+        map((res) =>
+          res.filter((category) => category.name.toLowerCase().includes(term))
+        )
+      );
+  }
 }
